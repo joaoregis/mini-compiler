3da68dba5311bb38c8baf602a366d5a7774a07d6
João Regis <joao.reegiis@gmail.com>
2019-04-25 18:48:53 -0300

    Update Compiler.java

--- src/br/com/joaoregis/compiler/Compiler.java
+++ src/br/com/joaoregis/compiler/Compiler.java
@@ -10,6 +10,7 @@ public class Compiler {
     private char program[];
     private String error;
     private String codeObjeto;
+    private int labelCount;
     
     public String getError() {
         return this.error;
@@ -24,10 +25,24 @@ public class Compiler {
         this.pos_token = -1;
         this.codeObjeto = "";
         this.error = "";
+        this.labelCount = 0;
     }
     
     private void nextChar() {
         this.pos_token++;
+        
+        while (
+            program[this.pos_token] == ' ' 
+            || program[this.pos_token] == '\r' 
+            || program[this.pos_token] == '\t' ) {
+
+            this.pos_token++;
+
+            if (this.pos_token >= program.length)
+                break;
+
+        }
+        
         if (this.pos_token < program.length) {
             token = program[this.pos_token];
         }
@@ -35,7 +50,7 @@ public class Compiler {
     
     public void init() {
         nextChar();
-        expression();
+        program();
     }
 
     private void match(char c) {
@@ -164,5 +179,71 @@ public class Compiler {
         emit("MOV AX, " + getNum());
     }
             
+    private void assignment() {
+        char name;
+        name = getName();
+        match('=');
+        expression();
+        emit("MOV [" + name + "], AX");
+    }
+    
+    private void program() {
+        block();
+        if (token != 'e')
+            error += "[ERROR] End was expected\n";
+        
+        emit("END");
+    }
+    
+    private void block() {
+        while(token != 'e' && token != 'l') {
+            switch(token) {
+                case 'i':
+                    doIf();
+                    break;
+                    
+                default:
+                    other();
+                    break;
+            }
+        }
+    }
+    
+    private void other() {
+        emit("# " + getName());
+    }
+    
+    private void condition() {
+        emit("# condition");
+    }
+    
+    private int newLabel() {
+        return labelCount++;
+    }
+    
+    private void postLabel(int lbl) {
+        emit("L" + lbl + ":");
+    }
+    
+    private void doIf() {
+        int l1, l2;
+        match('i');
+        condition();
+        l1 = newLabel();
+        l2 = l1;
+        emit("JZ L" + l1);
+        block();
+        
+        if (token == 'l') {
+            match('l');
+            l2 = newLabel();
+            emit("JMP L" + l2);
+            postLabel(l1);
+            block();
+        }
+
+        match('e');
+        postLabel(l2);
+    }
     
 }
